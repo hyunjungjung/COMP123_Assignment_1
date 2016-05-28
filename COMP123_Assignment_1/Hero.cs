6e5d64dc4aa84f4e36536c8a4e69cb672a40d8dd
Joanne Jung <hyunjung.joannjung@outlook.com>
2016-05-27 20:16:05 -0400

    Initial Commit

--- COMP123_Assignment_1/Hero.cs
+++ COMP123_Assignment_1/Hero.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /*
  * Author: Joanne (Hyunjung) Jung
  * Student # : 300432364
  * Date Modified: May 27, 2016
- * Description: Main method fror driver class Program for Assignment 1
+ * Description: Hero class for Assignment 1
  * Version: 0.0.1 - Initial Commit
  */
 namespace COMP123_Assignment_1
 {
-    class Program
+    class Hero
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
